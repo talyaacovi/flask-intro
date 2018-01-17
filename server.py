129c36e6a6d3f6fbfc282429f709d0c8777982b3
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-17 13:02:17 -0800

    completed most of assignment, missing Pattern Matching section

---
+++ server.py
@@ -0,0 +1,82 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+      <html>
+        Hi! This is the home page.<br>
+        <a href="/hello">Let's get started &#x2665</a>
+      </html>"""
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          Pick a compliment!
+            <select name="compliment">
+              <option value="oh-so-not-meh">Oh-so-not-meh</option>
+              <option value="rad">Rad</option>
+              <option value="ducky">Ducky</option>
+            </select>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    compliment = request.args.get("compliment")
+    # x = y
+    # compliment = choice(AWESOMENESS)
+    # print player, compliment
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """.format(player=player, compliment=compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True)
